88840f5b32e97e9403b1001ba32a5a7f4f34218a
Elliot <elliotgan@126.com>
2025-02-17 04:53:27 +0000

    javadoc and some small changes added

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -9,12 +9,19 @@ public class SimpleStringBuffer {
     private int sz;
     private int index;
 
+    /**
+     * A naive implementation of a text buffer using a string.
+     */
     SimpleStringBuffer() {
         this.str = "";
         this.index = 0;
         this.sz = 0;
     }
 
+    /**
+     * insert the ch in current index of buffer
+     * @param ch the ch that should be inserted
+     */
     public void insert(char ch) {
         char[] retch = new char[1];
         retch[0] = ch;
@@ -27,6 +34,10 @@ public class SimpleStringBuffer {
         this.index ++;
     }
 
+    /**
+     * delete the ch in current index of budder, if the
+     * index is at 0, do nothing.
+     */
     public void delete() {
         if (this.index == 0)
             return;
@@ -37,28 +48,56 @@ public class SimpleStringBuffer {
         this.index --;
     }
 
+    /**
+     * get the position of cursor
+     * @return int that represents position of cursor
+     */
     public int getCursorPosition() {
         return this.index;
     }
 
+    /**
+     * move cursor, which is the index, towards right
+     * if the index is  at rightmost position, do nothing.
+     */
     public void moveLeft() {
         if (this.index > 0)
             this.index --;
     }
 
+    /**
+     * move cursor, which is the index, towards left
+     * if the index is  at leftmost position, do nothing.
+     */
     public void moveRight() {
         if (this.index < this.sz)
             this.index ++;
     }
 
+    /**
+     * get the current size of buffer
+     * @return int that represents current size of buffer
+     */
     public int getSize() {
         return this.sz;
     }
 
-    public char getChar(int i) {
-        return (this.str).charAt(this.index);
+    /**
+     * get the char at specific position
+     * @param i int 
+     * @return the char at i
+     * @throws java.lang.InterruptedException 
+     */
+    public char getChar(int i) throws InterruptedException {
+        if((0 <= i) && (i < this.sz))
+          return (this.str).charAt(this.index);
+        throw new InterruptedException("int i not in range");
     }
 
+    /**
+     * return string stored in buffer
+     * @return string stored in buffer
+     */
     @Override
     public String toString() {
         return this.str;
